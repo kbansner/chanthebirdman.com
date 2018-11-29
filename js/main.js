e374498ff305f845732343ef9f57a5e445216b05
Kai Bansner <kai@bansner.com>
2018-11-28 21:39:32 -0800

    basic template
    Full screen video example
    
    Remove video, too big for github
    
    Adding in JS main to index
    
    Working with Chan's copy
    
    Scrolling version
    
    Add hamburger menu

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#message button");
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function() {
+// only functional if "loop" is removed
+vid.pause();
+// to capture IE10
+vidFade();
+});
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+
+/* A simple and scalable hamburger menu using css transitions. */
+var isActive = false;
+
+$('.js-menu').on('click', function() {
+	if (isActive) {
+		$(this).removeClass('active');
+		$('body').removeClass('menu-open');
+	} else {
+		$(this).addClass('active');
+		$('body').addClass('menu-open');
+	}
+
+	isActive = !isActive;
+});
