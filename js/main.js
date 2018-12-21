67b2094a28fb23f7118f2638b15b59f5361b94fa
Kai Bansner <kai@bansner.com>
2018-12-20 21:57:33 -0800

    Desktop video play button wired up

--- js/main.js
+++ js/main.js
@@ -1,50 +1,38 @@
 var vid = document.getElementById("bgvid");
-var message = document.getElementById("message");
-var pauseButton = document.querySelector("#videoButton");
-
-function vidFade() {
-  vid.classList.add("stop");
-}
+var pauseButton = document.querySelector("#play");
 
 vid.addEventListener('ended', function() {
-// only functional if "loop" is removed
-vid.pause();
-// to capture IE10
-vidFade();
+  // only functional if "loop" is removed
+  vid.pause();
+  vid.classList.add("stop");
 });
 
-
 pauseButton.addEventListener("click", function() {
-  vid.classList.toggle("stop");
   if (vid.paused) {
     vid.play();
     window.scrollTo(0,0);
-    pauseButton.innerHTML = "Pause";
+    vid.classList.remove("stop");
   } else {
     vid.pause();
-    pauseButton.innerHTML = "Play Video";
   }
 })
+
+
 $(document).ready(function(){
-  vid.pause();
-  message.classList.add('show');
+  $(window).scroll(function(){
+    var s = $(this).scrollTop()
+    //console.log('s', s);
+    if (s > 100) {
+      pauseButton.classList.add('hide');
+      vid.pause();
+    }
+    else {
+      pauseButton.classList.remove('hide');
+    }
+  })
 });
 
-/* A simple and scalable hamburger menu using css transitions. */
-var isActive = false;
-
-$('.js-menu').on('click', function() {
-	if (isActive) {
-		$(this).removeClass('active');
-		$('body').removeClass('menu-open');
-	} else {
-		$(this).addClass('active');
-		$('body').addClass('menu-open');
-	}
-
-	isActive = !isActive;
-});
-
-$('.draw-opener').on('click', function(){
-  $('.draw').toggleClass('open');
+var playButton = document.getElementById('play');
+playButton.addEventListener('click', function(){
+  playButton.classList.toggle('toggle');
 });
